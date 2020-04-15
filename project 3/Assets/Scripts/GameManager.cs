b16a911d5295e0bc0914767d81a29365b651f2ee
ishvara <adjodha1456@gmail.com>
2020-04-15 13:23:45 +0200

    no message

---
+++ project 3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] WoordenPrefabs;
+    private float InstantiationTimer = 2f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI GameOver;
+    // Start is called before the first frame update
+    void Start() 
+    {
+     
+    }
+
+    // Update is called once per frame
+
+
+
+    void Update()
+    {
+
+        score = 0;
+        UpdateScore(0);
+        int LetterIndex = Random.Range(0, WoordenPrefabs.Length);
+
+        InstantiationTimer -= Time.deltaTime;
+        if (InstantiationTimer <= 0)
+        {
+            Instantiate(WoordenPrefabs[LetterIndex], new Vector3(Random.Range(-200, 200), 100, 100), Quaternion.identity);
+            InstantiationTimer = 2f;
+        }
+
+
+
+
+        if (transform.position.y < -100)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "score = " + score;
+    }
+
+    public void StartGame()
+    {
+        score = 0;
+        UpdateScore(0);
+    }
+}
---
+++ project 3/Assets/Scripts/target.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    public ParticleSystem explosionParticle;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    private GameManager gameManager;
+    public int PointValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        var mousePos = Input.mousePosition;
+        score = 0;
+        UpdateScore(0);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        gameManager.UpdateScore(PointValue);
+        //Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "score = " + score;
+    }
+
+}
+
+
+
